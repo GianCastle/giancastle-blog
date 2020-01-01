22546964baf3c5c0f42961b73366d69a3208e695
Giancarlos Castillo <gc.c@outlook.com>
2020-01-01 14:25:27 -0400

    feat(config): adds GA tracking code

--- gatsby-config.js
+++ gatsby-config.js
@@ -65,7 +65,7 @@ module.exports = {
     {
       resolve: 'gatsby-plugin-google-analytics',
       options: {
-        trackingId: 'UA-XXXX-Y',
+        trackingId: 'UA-145854662-3',
         // Puts tracking script in the head instead of the body
         head: true,
         // IP anonymization for GDPR compliance
