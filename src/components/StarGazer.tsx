b4fd5cd4e4bb9f1847b21c388365f4da09e8ec18
Giancarlos Castillo <gc.c@outlook.com>
2020-05-08 00:27:17 -0400

    feat(component): adds StarGazer component

---
+++ src/components/StarGazer.tsx
@@ -0,0 +1,50 @@
+import Particles from 'react-particles-js';
+import React from 'react';
+
+export const StarGazer = () => (
+  <Particles
+    params={{
+      particles: {
+        number: {
+          value: 200,
+          density: {
+            enable: true,
+            value_area: 1700,
+          },
+        },
+        line_linked: {
+          enable: true,
+          opacity: 0.02,
+        },
+        move: {
+          direction: 'center',
+          speed: 0.05,
+        },
+        size: {
+          value: 1,
+        },
+        opacity: {
+          anim: {
+            enable: true,
+            speed: 1,
+            opacity_min: 0.05,
+          },
+        },
+      },
+      interactivity: {
+        events: {
+          onclick: {
+            enable: true,
+            mode: 'push',
+          },
+        },
+        modes: {
+          push: {
+            particles_nb: 1,
+          },
+        },
+      },
+      retina_detect: true,
+    }}
+  />
+);
