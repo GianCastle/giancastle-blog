6e97ea7e39cf47db138fb17333268a9c71a0b171
Giancarlos Castillo <gc.c@outlook.com>
2020-05-12 15:16:50 -0400

    refact: move stargazer configuratino to config directory

--- src/components/StarGazer.tsx
+++ src/components/StarGazer.tsx
@@ -1,50 +1,5 @@
 import Particles from 'react-particles-js';
 import React from 'react';
+import { starGazerConfig } from '../configs/stargazerConfig';
 
-export const StarGazer = () => (
-  <Particles
-    params={{
-      particles: {
-        number: {
-          value: 300,
-          density: {
-            enable: true,
-            value_area: 1700,
-          },
-        },
-        line_linked: {
-          enable: true,
-          opacity: 0.02,
-        },
-        move: {
-          direction: 'center',
-          speed: 0.05,
-        },
-        size: {
-          value: 1,
-        },
-        opacity: {
-          anim: {
-            enable: true,
-            speed: 1,
-            opacity_min: 0.05,
-          },
-        },
-      },
-      interactivity: {
-        events: {
-          onclick: {
-            enable: true,
-            mode: 'push',
-          },
-        },
-        modes: {
-          push: {
-            particles_nb: 1,
-          },
-        },
-      },
-      retina_detect: true,
-    }}
-  />
-);
+export const StarGazer = () => <Particles params={starGazerConfig} />;
