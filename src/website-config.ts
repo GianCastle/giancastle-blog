5a9ec7f3c3082419990a655af673f0c4c7056040
Giancarlos Castillo <gc.c@outlook.com>
2020-05-12 15:34:16 -0400

    chore: change footer text

--- src/website-config.ts
+++ src/website-config.ts
@@ -58,11 +58,12 @@ const config: WebsiteConfig = {
   twitter: 'https://twitter.com/',
   facebook: 'https://facebook.com/giancastle',
   showSubscribe: false,
-  mailchimpAction: 'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
+  mailchimpAction:
+    'https://twitter.us19.list-manage.com/subscribe/post?u=a89b6987ac248c81b0b7f3a0f&amp;id=7d777b7d75',
   mailchimpName: 'b_a89b6987ac248c81b0b7f3a0f_7d777b7d75',
   mailchimpEmailFieldName: 'MERGE0',
   googleSiteVerification: 'GoogleCode',
-  footer: 'Breathe in life. Breathe in, breathe out...',
+  footer: 'Breathe in life.',
 };
 
 export default config;
